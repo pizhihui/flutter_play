c34b2f0c6d0cd4517c96d32362ed97ad5257c154
zhihuipi <zhihuipi@lexin.com>
2019-07-26 13:54:05 +0800

    常见 widget 的练习

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:flutter/foundation.dart';
 
 //void main() => runApp(MyApp());
 
-void main(){
+void main() {
   runApp(new MyAPP());
 }
 
@@ -32,18 +32,113 @@ final ThemeData kDefaultTheme = new ThemeData(
 );
 
 class MyAPP extends StatelessWidget {
+
+  //用户名
+  String userName;
+
+  //密码
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "FriendlyChat",
-      theme: defaultTargetPlatform == TargetPlatform.iOS         //new
-          ? kIOSTheme                                              //new
+      theme: defaultTargetPlatform == TargetPlatform.iOS //new
+          ? kIOSTheme //new
           : kDefaultTheme,
-      home: ChatScreen(),
+      routes: {
+        '/first': (BuildContext context) => FirstPage(), //添加路由
+        '/second': (BuildContext context) => SecondPage(),
+      },
+      initialRoute: '/first',//初始路由页面为first页面
+      home: MyHomePage(),
     );
   }
 }
 
+//这是一个可改变的Widget
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('MaterialApp示例'),
+      ),
+      body: Center(
+        child: Text(
+          '主页',
+          style: TextStyle(fontSize: 28.0),
+        ),
+      ),
+    );
+  }
+}
+
+//第一个路由页面
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('这是第一页'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //路由跳转到第二个页面
+            Navigator.pushNamed(context, '/second');
+          },
+          child: Text(
+            '这是第一页',
+            style: TextStyle(fontSize: 28.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//第二个路由页面
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('这是第二页'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //路由跳转到第一个页面
+            Navigator.pushNamed(context, '/first');
+          },
+          child: Text(
+            '这是第二页',
+            style: TextStyle(fontSize: 28.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+//
+//class MyAPP extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: "FriendlyChat",
+//      theme: defaultTargetPlatform == TargetPlatform.iOS         //new
+//          ? kIOSTheme                                              //new
+//          : kDefaultTheme,
+//      home: ChatScreen(),
+//    );
+//  }
+//}
 
 //class MyApp extends StatelessWidget {
 //  // This widget is the root of your application.
